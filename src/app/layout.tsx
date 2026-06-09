2afb4505df5fcd15ccd84e2b5e7e6cdc4a7c6e7b
satyam06050 <krsatyam0605@gmail.com>
2026-06-09 14:26:21 +0530

    add application layout and navigation

---
+++ src/app/layout.tsx
@@ -0,0 +1,62 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Providers from "@/components/Providers";
+import { cn } from "@/lib/utils";
+import type { Metadata, Viewport } from "next";
+import { Calistoga, Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  weight: ["400", "500", "600", "700"],
+});
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
+
+export const metadata: Metadata = {
+  title: "Ted Thoughts.",
+  description: "My personal site to showcase my developer work and opinions.",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/icon.svg", type: "image/svg+xml" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/manifest.json",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable,
+          calistoga.variable,
+        )}
+      >
+        <Providers>
+          <Header />
+          <div className="mx-auto flex max-w-3xl flex-col px-8">
+            <main className="grow">{children}</main>
+          </div>
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  );
+}
